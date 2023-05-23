08720e6b75094118ab82e665350ce4ce27d35298
Benjamin James <benjames@mit.edu>
2023-05-23 10:51:32 -0400

    added python code

---
+++ miBrain/cpdb.py
@@ -0,0 +1,9 @@
+
+
+def run_cpdb(cpdb_file_path:str, meta_file_path:str, counts_file_path:str, deg_file:str, out_path:str=None):
+        from itertools import combinations
+        import os
+        import pandas as pd
+        from cellphonedb.src.core.methods import cpdb_degs_analysis_method, cpdb_statistical_analysis_method
+        from cellphonedb.utils import search_utils
+        return None
